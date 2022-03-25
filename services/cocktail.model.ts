d2f00d341f8f875ac4a282d550d3d061b90bcbfc
Charlotte-ui <55531877+Charlotte-ui@users.noreply.github.com>
2022-03-25 16:18:33 +0100

    Create cocktail.model.ts

---
+++ services/cocktail.model.ts
@@ -0,0 +1,8 @@
+export default class Cocktail {
+    thumbnail: string;
+    
+    constructor(public id: number, public name: string, public image: string, public instructions: string) {
+      this.thumbnail = image + '/preview';
+    }
+  }
+  
